99146eeb2004bc53aa8c989daa244888dafca839
SheheryarRaza <117226030+SheheryarRaza@users.noreply.github.com>
2025-07-02 14:05:57 +0500

    Create CodingSession

---
+++ CodingTracker.SheheryarRaza/CodingSession.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTracker.SheheryarRaza
+{
+    public class CodingSession
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        public void CalculateDuration()
+        {
+            if (EndTime < StartTime)
+            {
+                Console.WriteLine("Warning: End time is before start time. Duration set to 0.");
+                Duration = TimeSpan.Zero;
+            }
+            else
+            {
+                Duration = EndTime - StartTime;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Start: {StartTime:yyyy-MM-dd HH:mm}, End: {EndTime:yyyy-MM-dd HH:mm}, Duration: {Duration}";
+        }
+    }
+}
