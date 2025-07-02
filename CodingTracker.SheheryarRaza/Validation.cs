465d839d1ea3efc0c914a8107e653873b509c1ef
SheheryarRaza <117226030+SheheryarRaza@users.noreply.github.com>
2025-07-02 14:05:18 +0500

    Validations

---
+++ CodingTracker.SheheryarRaza/Validation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTracker.SheheryarRaza
+{
+    public class Validation
+    {
+
+        public const string DateTimeFormat = "yyyy-MM-dd HH:mm";
+        public static bool IsValidDateTime(string input, out DateTime parsedDateTime)
+        {
+
+            return DateTime.TryParseExact(input, DateTimeFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedDateTime);
+        }
+
+        public static bool IsValidInteger(string input, out int parsedInt)
+        {
+            return int.TryParse(input, out parsedInt);
+        }
+    }
+}
