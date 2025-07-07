7b66ccfc42320f3d474c7921aadee47550cd11aa
SheheryarRaza <117226030+SheheryarRaza@users.noreply.github.com>
2025-07-07 14:34:00 +0500

    removed unnessecary usings

--- CodingTracker.SheheryarRaza/DBContext.cs
+++ CodingTracker.SheheryarRaza/DBContext.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Dapper;
+﻿using Dapper;
 using Microsoft.Data.Sqlite;
-using Microsoft.Extensions.Configuration;
 
 namespace CodingTracker.SheheryarRaza
 {
